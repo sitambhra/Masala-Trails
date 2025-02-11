c599fdc125009bf7a9a3dd751240a041df2ea70a
Abhishek-Kr-Pandit <abhishekrpandit18@gmail.com>
2025-02-11 15:31:36 +0530

    Toggle to Dynamic Theme icon changes made

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from "react";
-import sunIcon from '../utils/sun.png';
-import moonIcon from '../utils/moon.png';
+import sunIcon from "../utils/sun.png";
+import moonIcon from "../utils/moon.png";
 
 function DarkModeToggle() {
-  // Get stored theme or default to 'light'
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
-
-  // Apply theme on mount
   useEffect(() => {
     const html = document.documentElement;
     if (theme === "dark") {
@@ -17,31 +14,20 @@ function DarkModeToggle() {
     localStorage.setItem("theme", theme);
   }, [theme]);
 
-  // Toggle function
+  // Function to change theme
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
   };
 
   return (
-    <div className="flex items-center gap-2">
-      <span className="text-gray-800 dark:text-gray-200"><img src={sunIcon} className="w-6 h-6 dark:w-6" alt="light" /></span>
-
-      {/* Toggle Switch */}
-      <div
-        className={`w-14 h-7 flex items-center bg-gray-300 dark:bg-gray-700 rounded-full p-1 cursor-pointer`}
-        onClick={toggleTheme}
-      >
-        <div
-          className={`bg-white dark:bg-gray-900 w-6 h-6 rounded-full shadow-md transform duration-300 ${
-            theme === "dark" ? "translate-x-7" : "translate-x-0"
-          }`}
-        />
-      </div>
-
-      <span className="text-gray-800 dark:text-gray-200"><img src={moonIcon} className="w-6 h-6 dark:w-6" alt="dark" /></span>
-    </div>
+    <button onClick={toggleTheme} className="w-8 h-8">
+      <img
+        src={theme === "light" ? moonIcon : sunIcon}
+        alt={theme === "light" ? "Light Mode" : "Dark Mode"}
+        className="w-full h-full"
+      />
+    </button>
   );
 }
 
 export default DarkModeToggle;
-
