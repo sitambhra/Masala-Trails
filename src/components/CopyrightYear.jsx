85764c4ac0efaa027ac29540bb4a2a45f86fbfed
Gaurav Pratap Singh <gs492979@gmail.com>
2025-02-26 13:10:57 +0000

    in copyright year section divider line and link added.

--- src/components/CopyrightYear.jsx
+++ src/components/CopyrightYear.jsx
@@ -6,7 +6,7 @@ const CopyrightYear = () => {
 
   return (
     <div className="w-full flex justify-center item-center p-4 mt-8">
-      <p className='text-md text-gray-400'>&copy; {currentYear} Masala Trails&trade; || All Rights Reserved.</p>
+      <p className='text-md text-gray-400'>&copy; {currentYear} <a href='https://masala-trails.vercel.app/'>Masala Trails&trade;</a> | All Rights Reserved.</p>
     </div>
   );
 };
