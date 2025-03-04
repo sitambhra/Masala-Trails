2d028a5efc3bf47b791560bd17344291bf00fc81
soumik7063 <ghataksoumik7063@gmail.com>
2025-03-04 21:06:53 +0530

    loading animation added

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Loader = () => {
     return (
-        <div className="fixed bg-black top-0 left-0 z-50 flex justify-center items-center w-full h-full">
-            <img className='h-10' src="./infinite-spinner.svg" alt="" />
+        <div className="fixed bg-black top-0 left-0 z-50 flex justify-center items-center w-full h-screen">
+            <img className='h-9' src="./infinite-spinner.svg" alt="" />
         </div>
     );
 };
